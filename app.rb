761be88cd6e8335ae4aa329b59433dd1f38cea1e
simbiri <122113428+simbiri@users.noreply.github.com>
2024-04-15 00:14:35 +0000

    phew

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get ("/square/new") do
+  erb(:square)
+end
+
+get("/square/results")do
+  @num = params.fetch("number").to_f
+  @num_sq = @num ** 2
+  erb(:squarres)
+end
+
+get ("/square_root/new") do
+  erb(:sqroot)
+end
+
+get ("/square_root/results") do
+  @num = params.fetch("user_number").to_f
+  @num_sq = Math.sqrt(@num)
+  erb(:sqrootres)
+end
+
+get ("/payment/new") do
+  erb(:payment)
+end
+
+get ("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_i
+  @princi = params.fetch("user_pv").to_i
+
+  rate = @apr/1200
+  n = @years * 12
+  numerator = rate * @princi
+  denominator = 1 - (1 + rate)**(-n)
+
+  @payment = (numerator/denominator).to_fs(:currency)
+  erb(:payres)
+end
+
+get ("/random/new") do
+  erb(:random)
+end
+
+get ("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  @rando = rand(@min..@max)
+
+  erb(:randomres)
 end
